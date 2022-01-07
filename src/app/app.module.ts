389efdf6bdd6f8065ede55ea902bc799824fa0cb
TMA\lxhong <lexuanhong913@gmail.com>
2022-01-07 17:48:33 +0700

    70%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,9 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,42 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, FormArray, Validators ,AbstractControl} from '@angular/forms';
+import { Subscriber } from 'rxjs';
+import { DataUserService } from '../Mock-data/data-user.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  form!:FormGroup
-  constructor( private fn:FormBuilder) { }
+  form!: FormGroup
+  dataUsers:any
+  check:any
+  constructor(private fn: FormBuilder,private dataUser:DataUserService) { }
 
   ngOnInit(): void {
-    this.form=this.fn.group({
-      name:[''],
-      Email:[],
-      tel:[],
+    this.form = this.fn.group({
+      name: ['',[Validators.required]],
+      Email: ['',[Validators.required,Validators.email]],
+      gender: [],
+      skills: this.fn.array([]),
       social: this.fn.group({
-        facebook:'',
-        twitter:''
+        facebook: '',
+        twitter: ''
       }),
       tels: this.fn.array([
-        this.fn.control('')
+        this.addTel()
       ])
     })
-  }
-  onSubmit(val:FormGroup){
-    console.log(val.value);
+    this.dataUser.getDatauser().subscribe(data=>this.dataUsers=data)
+ 
+
     
   }
+  onSubmit(val: FormGroup) {
+    console.log(val.value);
+
+  }
+
   get tels(): FormArray {
     return this.form.get('tels') as FormArray;
   }
-  
+
   addTel() {
-    this.tels.push(this.fn.control(''));
+    return this.fn.group({
+      tel: [],
+      address: []
+    })
   }
-  
+  additemtel() {
+    this.tels.push(this.addTel())
+  }
   removeTel(index: number) {
     this.tels.removeAt(index);
   }
-
+  handleChecked(e: any) {
+    const valueCheck: FormArray = this.form.get('skills') as FormArray
+    valueCheck.push(new FormControl(e.target.value))
+  }
 }
