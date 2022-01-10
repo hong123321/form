7d7c58730c21869aec148c18db4ab8f978baac6b
TMA\lxhong <lexuanhong913@gmail.com>
2022-01-10 11:40:54 +0700

    90%

--- src/app/Mock-data/data-user.service.ts
+++ src/app/Mock-data/data-user.service.ts
@@ -1,14 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { catchError, map, tap } from 'rxjs/operators';
+const httpOptions ={
+  headers: new HttpHeaders({'conten-type':'application/JSON'})
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DataUserService {
-  api:string= "https://61d7f81be6744d0017ba8879.mockapi.io/user/datausers"
+  api:string= "https://61d7f81be6744d0017ba8879.mockapi.io/user/datausers?id=1"
   constructor(private http:HttpClient) { }
   getDatauser():Observable<any>{
     return this.http.get<any>(this.api)
+  }
+  updatUser(data:any):Observable<any>{
+    return this.http.put<any>(this.api,data,httpOptions)
   }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, FormArray, Validators ,AbstractControl} from '@angular/forms';
 import { Subscriber } from 'rxjs';
 import { DataUserService } from '../Mock-data/data-user.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { error } from 'protractor';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -11,13 +13,14 @@ export class FormComponent implements OnInit {
   form!: FormGroup
   dataUsers:any
   check:any
-  constructor(private fn: FormBuilder,private dataUser:DataUserService) { }
+  data:any
+  constructor(private fn: FormBuilder,private dataUser:DataUserService,private demo:HttpClient) { }
 
   ngOnInit(): void {
     this.form = this.fn.group({
       name: ['',[Validators.required]],
       Email: ['',[Validators.required,Validators.email]],
-      gender: [],
+      gender: ["male"],
       skills: this.fn.array([]),
       social: this.fn.group({
         facebook: '',
@@ -35,6 +38,17 @@ export class FormComponent implements OnInit {
   onSubmit(val: FormGroup) {
     console.log(val.value);
 
+     this.data=val.value
+    // this.dataUser.updatUser(data)
+    this.demo.put<any>('https://61d7f81be6744d0017ba8879.mockapi.io/user/datausers?id=1',this.data).subscribe({
+      next:data=>{
+        console.log(data)
+      },
+      error : error =>{
+        console.error(error)
+        
+      }
+    })
   }
 
   get tels(): FormArray {
